92ca537d5e3a04f5686895b8ba6a24869bba9a81
Michał Bagrowski <bagrowski0001@gmail.com>
2023-03-29 09:15:37 +0200

    pierwsza wersja

---
+++ main.py
@@ -0,0 +1,15 @@
+print('\n')
+zakupy = {
+    'Jula':'młotek',
+    'Społem':'chleb',
+    'Gryvan':'lody'
+    }
+
+for sklep, towar in zakupy.items():
+    print(f"Idę do sklepu {sklep.title()} i kupię {towar.title()}.")
+
+counter = 0
+for towar in zakupy:
+    counter+=1
+print(f"W sumie kupuję {counter} produkty.")
+print('\n')
