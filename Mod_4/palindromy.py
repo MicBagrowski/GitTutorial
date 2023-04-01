14ef759840a44a5b3ba77b853bdfab78cbac54c3
Michał Bagrowski <bagrowski0001@gmail.com>
2023-04-01 15:01:59 +0200

    poprawa 1

--- Mod_4/palindromy.py
+++ Mod_4/palindromy.py
@@ -1,25 +1,22 @@
-print("\nTen program sprawdza, czy tekst jest palindromem\n")
-prompt = "Wpisz tekst, który chcesz sprawdzić\ni naciśnij Enter:\n "
-prompt +="(Jeśli nie chcesz sprawdzać więcej tekstów, wpisz 'koniec')   \n"
+def is_palindrome(word):
+    """Sprawdza czy wpisany tekst jest palindromem"""
+    word = [x for x in word.lower() if x.isalnum()]
+    reverse_word = word[::-1]
+    return word == reverse_word 
 
+def ask_user_for_words_in_loop_and_print_if_word_is_palindrome():
+    """to jest przypadek użycia - jeden z pomysłów na wykorzystanie definicji"""
+    print("\nTen program sprawdza, czy tekst jest palindromem\n")
+    prompt = """Wpisz tekst, który chcesz sprawdzić i naciśnij Enter. Jeśli nie chcesz sprawdzać więcej tekstów, wpisz 'koniec':  
+    """
+    while True:
+        word = input(prompt)
 
-active = True
-while active:
-    word = input(prompt)
+        if word == 'koniec':
+            break
+        if is_palindrome(word):
+            print("Twoje słowo to palindrom\n")
+        else:
+            print("\nTwoje słowo nie jest palindromem\n")
 
-    if word == 'koniec':
-        active = False
-    else:
-        def if_palindrome(word):
-            """Sprawdza czy wpisany tekst jest palindromem"""
-            word = word.replace(' ','')
-            word = word.lower()
-            palindrome = word[::-1];
-            return True if word == palindrome else False
-     
-    if (if_palindrome(word)):
-        print("Twoje słowo to palindrom\n")
-    else:
-        print("\nTwoje słowo nie jest palindromem\n")
-
-    if_palindrome(word)
+ask_user_for_words_in_loop_and_print_if_word_is_palindrome()
