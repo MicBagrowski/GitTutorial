5bf8857d5bd687ceefc3c4b4d4b0bccccc54358c
Michał Bagrowski <bagrowski0001@gmail.com>
2023-04-06 20:25:20 +0200

    poprawki w angielskim...

--- Mod_5/Zadanie_5_3.py
+++ Mod_5/Zadanie_5_3.py
@@ -3,41 +3,41 @@ print('\n')
 class BasicContact():
     """Wizytówka prywatna"""
     
-    def __init__(self, name, forname, phone, email):
+    def __init__(self, name, surname, phone, email):
         """Atrybuty opisujące wizytówkę prywatną"""
         self.name = name
-        self.forname = forname
+        self.surname = surname
         self.phone = phone
         self.email = email
 
     def contact(self):
         """Sformułowanie wyboru wizytówki"""
-        person_card = f"Wybieram numer prywatny {self.phone} i dzwonię do {self.name.title()} {self.forname.title()}."
+        person_card = f"Wybieram numer prywatny {self.phone} i dzwonię do {self.name.title()} {self.surname.title()}."
         print(person_card)
         
     def label_length(self):
         """Wyświetlenie długości imienia i nazwiska"""
-        print(f"Imię i nazwisko ma {len(self.name + self.forname)} znaków.")
+        print(f"Imię i nazwisko ma {len(self.name + self.surname)} znaków.")
 
 class BusinessContact():
     """Wizytówka służbowa"""
     
-    def __init__(self, name, forname, position, company, business_phone):
+    def __init__(self, name, surname, position, company, business_phone):
         """Atrybuty opisujące wizytówkę prywatną"""
         self.name = name
-        self.forname = forname
+        self.surname = surname
         self.position = position
         self.company = company
         self.business_phone = business_phone
 
     def contact(self):
         """Sformułowanie wyboru wizytówki"""
-        person_card = f" Wybieram numer służbowy {self.business_phone} i dzwonię do {self.name.title()} {self.forname.title()}."
+        person_card = f" Wybieram numer służbowy {self.business_phone} i dzwonię do {self.name.title()} {self.surname.title()}."
         print(person_card)
     
     def label_length(self):
         """Wyświetlenie długości imienia i nazwiska"""
-        print(f" Imię i nazwisko ma {len(self.name + self.forname)} znaków.")
+        print(f" Imię i nazwisko ma {len(self.name + self.surname)} znaków.")
 
 person_basic = BasicContact('Patrycja', 'Borkowska', +489991435, 'PatrycjaBorkowska@rhyta.com')
 person_basic.contact()
